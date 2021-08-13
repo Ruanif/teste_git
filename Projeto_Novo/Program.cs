fc5156311cd7f9e6bfb974d0b86aa699ccc48d40
Ruan Rodrigues <ruan.rodrigues@fitbank.com.br>
2021-08-13 12:09:02 -0300

    acrescentada a mensagem de boa tarde

--- Projeto_Novo/Program.cs
+++ Projeto_Novo/Program.cs
@@ -8,6 +8,7 @@ namespace Projeto_Novo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Boa tarde");
         }
     }
 }
