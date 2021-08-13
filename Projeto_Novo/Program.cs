f59d9191f63e6ac306f841873ffca47c15d2b52a
Ruan Rodrigues <ruan.rodrigues@fitbank.com.br>
2021-08-13 11:16:54 -0300

    Projeto iniciado, mostrando a mensagem Ola mundo

---
+++ Projeto_Novo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Novo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
